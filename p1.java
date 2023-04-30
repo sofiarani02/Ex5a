f59421514e34a99f22d123a8fc0f2c4eee92f1a7
sofiarani02 <92247026+sofiarani02@users.noreply.github.com>
2023-04-30 12:16:05 +0530

    p1.java

--- p1.java
+++ p1.java
@@ -8,17 +8,21 @@ public class p1 extends HttpServlet {
    private String message;
    public void init() throws ServletException {
       // Do required initialization
-      message = "Hello LICET";
+      message = "Hello, Welcome to the page!!";
    }
 
    public void doGet(HttpServletRequest request, HttpServletResponse response)
          throws ServletException, IOException {
+
       // Set response content type
       response.setContentType("text/html");
 
       // Actual logic goes here.
       PrintWriter out = response.getWriter();
+
+      out.println("<html><head><title>Welcome Page</title></head><body style='text-align: center;'>");
       out.println("<h1>" + message + "</h1>");
+      out.println("</body></html>");
    }
 
    public void destroy() {
